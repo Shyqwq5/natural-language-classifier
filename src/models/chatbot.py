c0c446b63e06bdf9dc79887624e2e8e3f7d18263
Shyqwq5 <shyqwq5@gmail.com>
2025-12-26 21:38:07 +0000

    feat(chatbot): add chat template separation and memory support using Qwen2.5-3B-Instruct

--- src/models/chatbot.py
+++ src/models/chatbot.py
@@ -1,25 +1,19 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+from copy import deepcopy
 class Chatbot:
     def __init__(self,model_name):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name)
-        self.system_prompt = ''
+        self.model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", dtype=torch.bfloat16)
+        self.history_message = [{"role": "system", "content": "",}]
         self.chat_history_ids = None
         self.arguments = {'do_sample':False,'max_new_tokens':32}
-        self.reset_history()
 
     def set_arguments(self,arguments):
         self.arguments = arguments
 
-    def add_system_prompt(self,prompt):
-        self.system_prompt = prompt
-        self.reset_history()
-
-
-    def reset_history(self):
-        self.chat_history_ids = self.encode_prompt('<|system|>\n' + self.system_prompt+'<|end|>\n')['input_ids']
-
+    def modify_system_prompt(self,prompt):
+        self.history_message[0]["content"] = prompt
 
     def encode_prompt(self,prompt):
         return self.tokenizer(prompt,return_tensors="pt")
@@ -28,22 +22,17 @@ class Chatbot:
         return self.tokenizer.decode(reply_ids,skip_special_tokens=True)
 
     def generate_reply(self,prompt):
-        prompt = '<|end|>\n' + '<|user|>\n' + prompt +'<|end|>\n' + "<|assistant|>\n"
-        encoded = self.encode_prompt(prompt)
-        input_ids = torch.cat((self.chat_history_ids, encoded['input_ids']),1)
+        messages = deepcopy(self.history_message)
+        messages.append({"role": "user", "content": prompt})
 
+        tokenized_chat = self.tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
 
-        outputs = self.model.generate(
-        input_ids = input_ids,
-        attention_mask = torch.ones_like(input_ids),
-        pad_token_id= self.tokenizer.eos_token_id,
-        **self.arguments,)
+        device = self.model.get_input_embeddings().weight.device
+        tokenized_chat = tokenized_chat.to(device)
 
-
-        new_tokens = outputs[0][input_ids.shape[1]:]
-        decoded_outputs = self.tokenizer.decode(new_tokens,skip_special_tokens=True)
-        self.chat_history_ids = outputs
-
+        outputs = self.model.generate(input_ids=tokenized_chat,**self.arguments)
+        new_tokens = outputs[0, tokenized_chat.shape[1]:]
+        decoded_outputs = self.decode_reply(new_tokens)
         return decoded_outputs
 
 
